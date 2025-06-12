b1ccef90900e709618f02b59c75951088f13e297
shy9803 <127914332+shy9803@users.noreply.github.com>
2025-06-12 13:56:54 +0900

    Update Navigation.js

--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -5,7 +5,7 @@ function Navigation(props) {
   return (
     <nav>
       <ul>
-        <li><Link to='/movieapi'>Home</Link></li>
+        <li><Link to='/'>Home</Link></li>
         <li><Link to='/intro'>Intro</Link></li>
         <li><Link to='/api'>API</Link></li>
         <li><Link to='/contact'>Contact US</Link></li>
